c1bc5a916855ffb231eb7226381b1b46e4b696a0
Arhamkhan45 <karham45@gmail.com>
2024-09-01 01:10:01 +0530

    Added metadata on page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ import Navbar from "./_components/home/layout/Navbar";
 import Hero from "./_components/home/layout/Hero";
 import Highlight from "./_components/home/layout/Highlight";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ARM Apple | Home",
+  description: "Developed By Arham Ullah khan",
+};
+
 export default function Home() {
   return (
     <div className="bg-black">
